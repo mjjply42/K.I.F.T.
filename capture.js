cf0ad3fe5fea190114f4688c7719d2c006920467
Matthew Jones <majones@e1z2r5p5.42.us.org>
2019-01-29 15:19:31 -0800

    first commit

---
+++ capture.js
@@ -0,0 +1,50 @@
+let preview = document.getElementById("preview");
+let recording = document.getElementById("recording");
+let startButton = document.getElementById("startButton");
+let stopButton = document.getElementById("stopButton");
+let downloadButton = document.getElementById("downloadButton");
+
+const constraints = {audio: true, video: false};
+
+let recordingTimeMS = 4000;
+
+function wait(delayInMS) {
+  return new Promise(resolve => setTimeout(resolve, delayInMS));
+}
+function startRecording(stream, lengthInMS) {
+  let recorder = new MediaRecorder(stream);
+  let data = [];
+ 
+  recorder.ondataavailable = event => data.push(event.data);
+  recorder.start();
+ 
+  let stopped = new Promise((resolve, reject) => {
+    recorder.onstop = resolve;
+    recorder.onerror = event => reject(event.name);
+  });
+
+  let recorded = wait(lengthInMS).then(
+    () => recorder.state == "recording" && recorder.stop()
+  );
+ 
+  return Promise.all([
+    stopped,
+    recorded
+  ])
+  .then(() => data);
+}
+function stop(stream) {
+  stream.getTracks().forEach(track => track.stop());
+}
+startButton.addEventListener("click", function() {
+  navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+    preview.srcObject = stream;
+  }).then(() => startRecording(preview.captureStream(), recordingTimeMS))
+  .then (recordedChunks => {
+    let recordedBlob = new Blob(recordedChunks, { type: "audio/ogg" });
+    recording.src = URL.createObjectURL(recordedBlob);
+  })
+}, false);
+stopButton.addEventListener("click", function() {
+  stop(preview.srcObject);
+}, false);
