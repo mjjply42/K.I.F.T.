c980a08dedeab738e9587d4908c6455cf393db38
Matthew Jones <majones@e1z3r6p21.42.us.org>
2019-02-11 17:21:40 -0800

    updated Content Type for Files

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 	"flag"
 	"fmt"
 	"html/template"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"strings"
 )
 
 //var serverDirectory string
@@ -37,5 +40,30 @@ func main() {
 }
 
 func handler(res http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path[1:]
+	log.Println(path)
+	data, err := ioutil.ReadFile(string(path))
+
+	if err == nil {
+		var contentType string
+
+		if strings.HasSuffix(path, ".css") {
+			contentType = "text/css"
+		} else if strings.HasSuffix(path, ".html") {
+			contentType = "text/html"
+		} else if strings.HasSuffix(path, ".js") {
+			contentType = "application/javascript"
+		} else if strings.HasSuffix(path, ".png") {
+			contentType = "image/png"
+		} else if strings.HasSuffix(path, ".svg") {
+			contentType = "image/svg+xml"
+		} else {
+			contentType = "text/plain"
+		}
+
+		res.Header().Add("Content Type", contentType)
+		res.Write(data)
+	}
+
 	http.ServeFile(res, req, fmt.Sprintf("%s", "index.html"))
 }
